a86f81a0c340d21eeaec8ee09196ce08d7cb9ebc
Minh Đức <vmd.vuminhduc2002@gmail.com>
2024-04-29 20:41:38 +0700

    UpdateFE

---
+++ app/build.gradle.kts
@@ -0,0 +1,90 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
+    id("kotlin-kapt")
+}
+
+kapt {
+    correctErrorTypes = true
+}
+
+android {
+    namespace = "com.minhduc202.musicapp"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.minhduc202.musicapp"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_18
+        targetCompatibility = JavaVersion.VERSION_18
+    }
+    kotlinOptions {
+        jvmTarget = "18"
+    }
+    dataBinding {
+        enable = true
+    }
+    viewBinding {
+        enable = true
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+    //Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // Firebase
+    implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
+    implementation("com.google.firebase:firebase-database")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-storage")
+
+    implementation("com.airbnb.android:epoxy:5.1.4")
+    // Add the annotation processor if you are using Epoxy's annotations (recommended)
+    kapt("com.airbnb.android:epoxy-processor:5.1.4")
+    // Layout error alignment library for horizontal epoxy model
+    implementation("com.github.rubensousa:gravitysnaphelper:2.2.2")
+
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    // scalable dp, sp library
+    implementation("com.intuit.sdp:sdp-android:1.1.1")
+
+    // media library
+    implementation("androidx.media:media:1.7.0")
+
+    // lib for lottie animation
+    implementation("com.airbnb.android:lottie:6.4.0")
+}
---
+++ app/src/main/java/com/minhduc202/musicapp/ui/custom/ClickFriendlyMotionLayout.kt
@@ -0,0 +1,32 @@
+package com.minhduc202.musicapp.ui.custom
+
+import android.content.Context
+import android.util.AttributeSet
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewConfiguration
+import androidx.constraintlayout.motion.widget.MotionLayout
+
+class ClickFriendlyMotionLayout: MotionLayout {
+    var startTime = 0L
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    override fun onInterceptTouchEvent(event: MotionEvent): Boolean {
+//        Log.e("ANCUTKO", event.action.toString())
+//        if (event.action == MotionEvent.ACTION_DOWN) {
+//            startTime = event.eventTime
+//        }
+//
+//        if (event.action == MotionEvent.ACTION_MOVE && event.eventTime.minus(startTime) >= 80) {
+//            return super.onInterceptTouchEvent(event)
+//        }
+
+        return false
+    }
+}
